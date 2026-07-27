f66a387774a4b4940044a6b2d2e051a77baf7689
Guillermo <trolo.molon@gmail.com>
2026-07-27 12:00:54 +0100

    Agregar soporte para traducciones en inglés y español

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,11 @@ export default defineConfig({
     vite: {
     plugins: [tailwindcss()],
   },
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es' ,'en'],
+    routing: {
+        prefixDefaultLocale: true,
+    }
+  }
 });
---
+++ src/i18n/index.ts
@@ -0,0 +1,30 @@
+import es from './en.json';
+import en from './es.json';
+
+export const languages = {
+  es,
+  en,
+} as const;
+
+export const defaultLang = 'es';
+
+export type Lang = keyof typeof languages;
+
+
+export type TranslationKey = Exclude<keyof typeof es, `_${string}`>;
+
+export function useTranslations(lang: Lang) {
+  return function t(
+    key: TranslationKey,
+    vars: Record<string, string | number> = {},
+  ): string {
+    const dict = languages[lang] || languages[defaultLang];
+    const val =
+      (dict as Record<string, string>)[key] ||
+      (languages[defaultLang] as Record<string, string>)[key];
+
+    const plantilla = val || key;
+
+    return plantilla.replace(/\{(\w+)\}/g, (_, v) => String(vars[v] ?? ''));
+  };
+}
